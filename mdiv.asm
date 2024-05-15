b76815d7902160ab736ba112d432ad227d1f6514
ziara1 <143950916+ziara1@users.noreply.github.com>
2024-05-15 23:55:36 +0200

    Add files via upload

--- mdiv.asm
+++ mdiv.asm
@@ -27,40 +27,51 @@ mdiv:
     inc r8
     dec r9b                         ; jesli r9b bedzie = 0 to x i y taki sam znak
 
-
 .check_x:
     mov rax, [rdi + rcx * 8 - 8]    ; laduje x do rax x[n-1]
     test rax, rax
     jns .loop                       ; jesli x >= 0 to skok
     inc r9b
-    inc r10b                        ; zeby wiedziec czy reszte zmieniac na -1
+    ;inc r10b                       ; zeby wiedziec czy reszte zmieniac na -1
 
 .reverse_x:
-
-
+    xor r11, r11
+    stc
+.reverse_loop:
+    not qword [rdi + r11 * 8]
+    adc qword [rdi + r11 * 8], 0
+    inc r11
+    cmp rsi, r11
+    jne .reverse_loop
+    test r10b, r10b
+    jz .exit
+    inc r10b
 
 .loop:
     dec rcx
     mov rax, [rdi + rcx * 8]
     div r8
     mov [rdi + rcx * 8], rax
-    test rcx, rcx               ; moze niepotrzebne
+    test rcx, rcx                   ; moze niepotrzebne
     jnz .loop
-    mov r11, rdx                ; przenosze reszte //moze niepotrzebne?
+    ;mov r11, rdx                   ; przenosze reszte //moze niepotrzebne?
 
 .negative_remainder:
     test r10b, r10b
-    jnz .negative_product
-    not r11
-    inc r11
+    jz .negative_product
+   ;not r11
+   not rdx
+   ;inc r11
+   inc rdx
 
 .negative_product:
-    test r9b, r9b
+    test r10b, r10b                 ; bo jesli r10b zmieniony tzn ze x zmieniony
     jz .reverse_x
 
 .exit:
-    mov rax, r11                ; w r11 jest remainder
-    ret                         ; wynik zwracam w rax
+   ;mov rax, r11                    ; w r11 jest remainder
+   mov rax, rdx
+    ret                             ; wynik zwracam w rax
 
 
 .handle_sigfpe:
