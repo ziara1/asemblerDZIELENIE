314f6faa63c26a17ccc504166f759caca076d9ef
ziara1 <143950916+ziara1@users.noreply.github.com>
2024-05-17 08:58:48 +0200

    Add files via upload

--- mdiv.asm
+++ mdiv.asm
@@ -18,7 +18,7 @@ mdiv:
 
 .check_y:
     test r8, r8
-    jns .check_x                    ; jesli y > 0
+    jns .check_x                    ; jesli y > 0 to  skacze
     not r8                          ; jesli y < 0 to zmieniam go na -y
     inc r8
     dec r9b                         ; jesli r9b bedzie = 0 to x i y taki sam znak
@@ -27,12 +27,12 @@ mdiv:
     mov rax, [rdi + rcx * 8 - 8]    ; laduje x do rax x[n-1]
     test rax, rax
     jns .loop                       ; jesli x >= 0 to skok
-    inc r9b
-    inc r10b                       ; zeby wiedziec czy reszte zmieniac na -1
+    inc r9b                         ; jesli r9b bedzie = 0 to x i y taki sam znak
+    inc r10b                        ; zeby wiedziec czy reszte zmieniac na -1
 
 .reverse_x:
     xor r11, r11
-    mov rax, 1
+    mov rax, 1                      ; bo w pierwszej komorce dodaje 1 (not i inc)
 .reverse_loop:
     not qword [rdi + r11 * 8]
     add qword [rdi + r11 * 8], rax
@@ -40,34 +40,35 @@ mdiv:
     inc r11
     cmp r11, rsi
     jne .reverse_loop
-    test rcx, rcx                     ; bo jesli rcx = 0 to loop juz byl wykonany
+    test rcx, rcx                   ; bo jesli rcx = 0 to loop juz byl wykonany
     jz .exit
 
 .loop:
     dec rcx
-    mov rax, [rdi + rcx * 8]
+    mov rax, [rdi + rcx * 8]        ; dzielenie rdx:rax przez r8, w rdx reszta
     div r8
     mov [rdi + rcx * 8], rax
     test rcx, rcx                   ; moze niepotrzebne
     jnz .loop
 
-.negative_remainder:
+.negative_remainder:                ; jesli trzeba zmienic reszte na ujemna
     test r10b, r10b
     jz .negative_product
     not rdx
     inc rdx
 
-.negative_product:
-    test r9b, r9b                 ; bo jesli r10b zmieniony tzn ze x zmieniony
+.negative_product:                  ; jesli trzeba zmienic iloraz na ujemny
+    test r9b, r9b                   
     jnz .reverse_x
-    mov rax, [rdi + rsi * 8 - 8]
-    test rax, rax
-    js .handle_sigfpe
+    mov rax, [rdi + rsi * 8 - 8]    ; jesli iloraz powinien byc dodatni, czyli 
+    test rax, rax                   ; nie zmieniamy go na ujemny, ale jest ujemny
+    js .handle_sigfpe               ; tzn ze wystapil overflow
 
 .exit:
-    mov rax, rdx                    ; w r11 jest remainder
-    ret                             ; wynik zwracam w rax
+    mov rax, rdx                    ; w rdx zostala reszta z dzielenia
+    ret                            
 
 .handle_sigfpe:
-    div r9b
+    div r9b                         ; bo r9b = 0 wtedy
 
+; POLSKIE ZNAKI
